f13df187d397d4e6ce6b2569b2503fa0215b85c0
ralfspoeth <ralf.spoeth@gmail.com>
2026-04-22 09:50:02 +0200

    prep as mojo

--- src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module io.github.ralfspoeth.log.weaver {
-    requires static maven.plugin.api;
-    requires static maven.plugin.annotations;
-    requires static io.github.ralfspoeth.log.api;
-    exports io.github.ralfspoeth.log.weaver;
-}
