bcbdf2e98dc372427147300b445251056d90dc23
ralfspoeth <ralf.spoeth@gmail.com>
2026-04-22 09:46:53 +0200

    prep as mojo

--- src/main/java/io/github/ralfspoeth/log/weaver/LoggingWeaver.java
+++ src/main/java/io/github/ralfspoeth/log/weaver/LoggingWeaver.java
@@ -1,4 +1,15 @@
 package io.github.ralfspoeth.log.weaver;
 
-public class LoggingWeaver {
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name="logging-weaver", defaultPhase = LifecyclePhase.PROCESS_CLASSES)
+public class LoggingWeaver extends AbstractMojo {
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,6 @@
 module io.github.ralfspoeth.log.weaver {
+    requires static maven.plugin.api;
+    requires static maven.plugin.annotations;
+    requires static io.github.ralfspoeth.log.api;
     exports io.github.ralfspoeth.log.weaver;
 }
